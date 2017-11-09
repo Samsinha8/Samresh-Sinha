68bf891b082dc9c0b07f3ced9d3439a4714e2278
sam749382 <sam.749382@gmail.com>
2017-11-10 00:20:02 +0530

    added pic and background to homepage

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <i className="fa fa-bars" />
-          <h2 className="name">Samresh Kumar Sinha</h2>
+          <h1 className="name">Samresh Kumar Sinha</h1>
           <a className="github" href="https://github.com/Samsinha8"><i className="fa fa-github" /></a>
           <a className="linked" href="https://www.linkedin.com/in/samresh-kumar-sinha-84393b146/"><i className="fa fa-linkedin" /></a>
         </header>
         <div className="navbar">
           <ul className="list">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Portfolios</li>
-            <li>Contact</li>
+            <li className="ignore">Home</li>
+            <li className="dontignore">About</li>
+            <li className="dontignore">Skills</li>
+            <li className="dontignore">Portfolios</li>
+            <li className="dontignore">Contact</li>
           </ul>
         </div>
-        <div className="content">
-        </div>
+        <div className="content" />
+        <img className="myimage" src="/images/myimage.jpg" alt="" />
+        <h1 className="hi">Hi ! I am Samresh</h1>
       </div>
     );
   }
